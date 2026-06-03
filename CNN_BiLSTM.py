8afb61bb955010e6e387afc32a850605bc511424
SulaimanS <95322305+SulaimanS11@users.noreply.github.com>
2026-06-03 13:37:08 -0400

    adding file for preliminary CNN-BiLSTM model

---
+++ CNN_BiLSTM.py
@@ -0,0 +1,14 @@
+﻿import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import models, layers, callbacks
+import numpy as np
+from pathlib import Path
+
+X_train_path = Path("data") / "prepared" / "X_train.npy"
+Y_train_path = Path("data") / "prepared" / "y_train.npy"
+
+X_train = np.load(X_train_path)
+y_train = np.load(Y_train_path)
+
+model = keras.Sequential()
+
